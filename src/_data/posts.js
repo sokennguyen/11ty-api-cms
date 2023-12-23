79e094776c0d9cbf89c556a71b1f8662e96b2f9a
soken <nsongkien@gmail.com>
2023-12-23 18:02:41 +0200

    innit

--- src/_data/posts.js
+++ src/_data/posts.js
@@ -3,8 +3,8 @@ const { default: axios } = require('axios');
 module.exports = async () => {
   try {
     const res = await axios.get('http://localhost:1337/api/posts');
-    console.log(res.data.data)
-    return res.data.data;
+    console.log(res.data.data.map(post=>post.attributes))
+    return res.data.data.map(post=>post.attributes);
   } catch (error) {
   }
 };
